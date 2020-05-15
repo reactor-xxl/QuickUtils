b4e373a8016c6c768b85d802bed48ddc937dbc68
reactor-xxl <reactor.xxl@gmail.com>
2020-05-14 19:49:20 -0700

    adds basic directory listing capability

--- src/FileLister.cpp
+++ src/FileLister.cpp
@@ -1,1 +1,51 @@
 #include "FileLister.hpp"
+
+
+
+FileLister::FileLister( const Path& path )
+	: m_listDirectory{ path } {;}
+
+
+std::vector<Path> FileLister::generateList() const
+{
+	std::vector<Path> fileListing;
+
+	if ( !m_listDirectory.empty() )
+	{
+		std::filesystem::directory_iterator dit( m_listDirectory );
+		for ( auto entry : dit )
+		{
+			auto p = entry.path();
+			if ( std::filesystem::is_regular_file( p ) )
+			{
+				if ( m_option_fullPath )
+					fileListing.push_back( p );
+				else
+					fileListing.push_back( p.filename() );
+
+			}
+
+		}
+	}
+	return fileListing;
+}
+
+
+void	FileLister::setDirectory( const Path& path )
+{
+	m_listDirectory = std::filesystem::absolute( path.parent_path() );
+}
+
+Path	FileLister::getDirectory() const
+{
+	return m_listDirectory;
+}
+
+FileLister&	FileLister::fullPath( bool b )
+{
+	m_option_fullPath = b;
+	return *this;
+}
+
+
+//void	FileLister::setExtensions( const std::vector<Path>& );
--- src/FileLister.hpp
+++ src/FileLister.hpp
@@ -8,26 +8,17 @@
 class FileLister
 {
 public:
-	std::vector<Path> generateList();
-
+	FileLister( const Path& path = std::filesystem::current_path() );
+	std::vector<Path> generateList() const;
 	void	setDirectory(const Path&);
 	Path	getDirectory() const;
 	void	setExtensions( const std::vector<Path>& );
 
+	FileLister&	fullPath(bool);
 
-/*
-directory
-file listing settings
-returns a vector of path
-
-in path
-out path
-options
-
-*/
-
-	Path	m_listDirectory;
-
+protected:
+	Path	m_listDirectory{};
+	bool	m_option_fullPath{ false };
 };
 
 
